55b2c3a8eb86fc4b408584fa51b212f3ba3fa43b
Erikstellet <erikstellet@gmail.com>
2021-02-20 22:55:22 -0300

    Profiles, Lists Profiles e Modal Overlay

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,102 @@
+// DATABASE
+const profiles = 
+[
+    { 
+        name: 'Bianca',
+        imgUrl: 'https://i.imgur.com/kJCWUAo.jpg',
+        followers: 10,
+        following: 50,
+        description:  "ABC"
+    },
+    { 
+        name: 'Ana',
+        imgUrl: 'https://i.imgur.com/kJCWUAo.jpg',
+        followers: 10,
+        following: 50,
+        description:  "ABC"
+    },
+    {
+        name: 'Bruna',
+        imgUrl: 'https://i.imgur.com/kJCWUAo.jpg',
+        followers: 10,
+        following: 50,
+        description:  "ABC"
+    },
+    { 
+        name: 'Alana',
+        imgUrl: 'https://i.imgur.com/kJCWUAo.jpg',
+        followers: 10,
+        following: 50,
+        description:  "ABC"
+    },
+    {
+        name: 'Maria',
+        imgUrl: 'https://i.imgur.com/kJCWUAo.jpg',
+        followers: 10,
+        following: 50,
+        description:  "ABC"  
+    },
+    { 
+        name: 'Julia',
+        imgUrl: 'https://i.imgur.com/kJCWUAo.jpg',
+        followers: 10,
+        following: 50,
+        description:  "ABC"  
+    },
+]
+
+// OPEN MODAL
 let modal = 
 {
+    openModalOverlay()
+    {
+        document.querySelector('.modal-overlay').classList.toggle('invisible')
+    },
     openPlus()
     {
-        document.querySelector('.modal-overlay').classList.toggle('invisible')
-        document.querySelector('.btn-plus').classList.add('z-50')
+        modal.openModalOverlay()
+        document.querySelector('.modal-plus').classList.toggle('hidden')
+        document.querySelector('.btn-plus').classList.toggle('z-50')
     },
-    /*
     openHand()
     {
-        document.querySelector('.modal-overlay').classList.toggle('invisible')
-        document.querySelector('.btn-plus').classList.add('z-50')
+        modal.openModalOverlay()
+        document.querySelector('.modal-hand').classList.toggle('hidden')
+        
     },
-    open()
+    openBio()
     {
-        document.querySelector('.modal-overlay').classList.toggle('invisible')
-        document.querySelector('.btn-plus').classList.add('z-50')
-    },*/
+        modal.openModalOverlay()
+        document.querySelector('.modal-bio').classList.toggle('hidden')
+        document.querySelector('.modal-bio').classList.toggle('flex')
+    },
 }
+
+// INNER HTML
+const DOM = 
+{
+    profileContainer: document.querySelector(".modal-p-search"),
+
+    createProfile(profile)
+    {
+        const div = document.createElement('div');
+        div.innerHTML = DOM.innerHTMLProfile(profile)
+
+        DOM.profileContainer.appendChild(div)
+    },
+    innerHTMLProfile(profiles)
+    {
+        const profile =  
+                    `<div class="">
+                        <div class="">
+                            <img class="h-16 my-1 mx-2" src="${profiles.imgUrl}">
+                            <img class="" src="">
+                        </div>
+                            <p>${profiles.name}</p>
+                     </div>`
+
+       return profile             
+    },
+}
+
+profiles.forEach(profile => DOM.createProfile(profile))
